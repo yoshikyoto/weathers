6153a765b1019f78a3d7382f1a76bc8e68dfaee5
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-09-21 15:37:39 +0900

    get request

--- src/Yahoo/Api.php
+++ src/Yahoo/Api.php
@@ -2,17 +2,25 @@
 
 namespace Yoshikyoto\Weather\Yahoo;
 
-use GuzzleHttp\Client;
-
 class Api
 {
-    public function __construct()
-    {
-        
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $client;
+    
+    public function __construct(
+        string $appId
+    ){
+        $this->client = new \GuzzleHttp\Client([
+            'base_uri' => 'https://weather-ydn-yql.media.yahoo.com',
+            'headers' => [
+            ],
+        ]);
     }
     
     public function get()
     {
-        
+        $this->client->get('/forecastrss');
     }
 }
