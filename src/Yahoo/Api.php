b134453eda869c94d2e3da4c859273e656f612aa
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-09-21 18:54:30 +0900

    これで叩けるはずではある

--- src/Yahoo/Api.php
+++ src/Yahoo/Api.php
@@ -2,6 +2,10 @@
 
 namespace Yoshikyoto\Weather\Yahoo;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
+
 class Api
 {
     /**
@@ -9,18 +13,44 @@ class Api
      */
     private $client;
     
+    /**
+     * @var string Client ID が Consumery Key のこと
+     */
+    private $consumerKey;
+    
+    private $oauth;
+    
     public function __construct(
-        string $appId
+        string $appId,
+        string $consumerKey,
+        string $consumerSecret
     ){
-        $this->client = new \GuzzleHttp\Client([
+        $oauth = new Oauth1([
+            'consumer_key' => $consumerKey,
+            'soncumer_secret' => $consumerSecret,
+            'signature_method' => Oauth1::SIGNATURE_METHOD_HMAC,
+        ]);
+        
+        $handlerStack = HandlerStack::create();
+        $handlerStack->push($oauth);
+        
+        $this->client = new Client([
             'base_uri' => 'https://weather-ydn-yql.media.yahoo.com',
             'headers' => [
+                'X-Yahoo-App-Id' => $appId,
             ],
+            'handler' => $handlerStack,
+            'debug' => true,
         ]);
     }
     
     public function get()
     {
-        $this->client->get('/forecastrss');
+        $this->client->get('/forecastrss', [
+            'auth' => 'oauth',
+            'query' => [
+                'format' => 'json',
+            ],
+        ]);
     }
 }
